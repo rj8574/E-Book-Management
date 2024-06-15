89303ae4bed7e78649db6dac6b9a97d2cbb78afd
Ritik Thakur <hritikthakur1947@gmail.com>
2024-06-15 19:29:23 +0530

    add register validation

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,7 +3,9 @@ import createHttpError, { HttpError } from "http-errors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 
+
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
   const error = createHttpError(400, `something went wrong`);
--- Backend/src/users/userController.ts
+++ Backend/src/users/userController.ts
@@ -1,6 +1,15 @@
+import createHttpError, { HttpError } from 'http-errors';
 import { Response, NextFunction, Request } from "express";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    const {name , email, password }=req.body
+    if(!name || !email || !password)
+    {
+        const error = createHttpError(400 ,"All fields are mandatory")
+        return next(error)
+    }
+    
+
   res.json({ message: "user registerd" });
 };
 
