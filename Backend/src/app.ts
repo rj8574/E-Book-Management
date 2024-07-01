cfb294ab185f4ebf5e2f40c4e777002dbcec4cf2
Ritik Thakur <hritikthakur1947@gmail.com>
2024-07-01 23:14:50 +0530

    Add cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,9 +3,13 @@ import createHttpError  from "http-errors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
-
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+app.use(cors({
+  origin: config.frontendDomain
+}))
 app.use(express.json())
 
 app.get("/", (req, res) => {
