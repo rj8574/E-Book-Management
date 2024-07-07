72ff773e80f791201b121badbc8252757911d115
Ritik Thakur <hritikthakur1947@gmail.com>
2024-07-08 03:39:58 +0530

    Add book page

--- Frontend/elib-client-app/src/app/(home)/components/BookCard.tsx
+++ Frontend/elib-client-app/src/app/(home)/components/BookCard.tsx
@@ -10,7 +10,7 @@ const BookCard=({book}:{book:Book})=>{
             <div>
                 <h2 className="line-clamp-2 text-xl font-bold text-primary-600 text-balance">{book.title}</h2>
                 <p className="font-bold text-purple-900 mt-1">{book.author.name}</p>
-                <Link href={`book/${book._id}`} 
+                <Link href={`/books/${book._id}`} 
                 className="py-1 px-2 rounded border border-primary-500 mt-5 inline-block text-primary-500 font-medium text-sm
                  hover:border-primary-100 hover:bg-primary-100 transition">
                 Read More</Link>
