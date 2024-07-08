802fbbba1f239eab614be8bd2defc51eb2eef526
Ritik Thakur <hritikthakur1947@gmail.com>
2024-07-09 01:05:34 +0530

    add suspense

--- Frontend/elib-client-app/src/app/(home)/components/bookList.tsx
+++ Frontend/elib-client-app/src/app/(home)/components/bookList.tsx
@@ -2,11 +2,19 @@ import { Book } from "../../../types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({books}:{books:Book[]}) => {
+const BookList = async () => {
+
+    const response = await fetch(`${process.env.BASE_BAKEND__URL}/books` ,{ cache: 'no-store' })
+    if(!response.ok)
+      {
+        return new Error('an error occuered while fetching the books')
+      }
+      const books =await response.json()
+      console.log('books:',books);
     return (
         <div className="grid grid-cols-3 gap-8 md:grid-3 max-w-7xl mx-auto mb-10">
             {
-                books.map((book) => (
+                books.map((book:Book) => (
                     <BookCard key={book._id} book={book}/>
                 ))
             }
--- Frontend/elib-client-app/src/app/(home)/page.tsx
+++ Frontend/elib-client-app/src/app/(home)/page.tsx
@@ -1,21 +1,20 @@
-import React from "react"
+import React, { Suspense } from "react"
 import Image from 'next/image';
 import Banner from "./components/Banner";
 import BookList from "./components/bookList";
+import Loading from "@/components/Loading";
+
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BASE_BAKEND__URL}/books` ,{ cache: 'no-store' })
-  if(!response.ok)
-    {
-      return new Error('an error occuered while fetching the books')
-    }
-    const books =await response.json()
-    console.log('books:',books);
+ 
     
   return (
     <>
     <Banner />
-    <BookList books={books}/>
+    <Suspense fallback={<Loading/>}>
+       <BookList/>
+    </Suspense>
+    
     </>
   )
 }
